04f080ff0bc99c03995855370fdc6d735c3a0232
jcanaviri <jcanaviri20@gmail.com>
2021-07-06 12:38:23 -0400

    Ejercicio 6 parte 2

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -5,3 +5,10 @@ let num1 = parseInt(prompt('De que numero quieres la tabla', 1));
 for (let i = 1; i <= 10; i++) {
   document.write(`<p>${num1} x ${i} = ${num1 * i}</p>`);
 }
+
+// Todas las tablas de multiplicar
+for (let j = 1; j <= 10; j++) {
+  for (let i = 1; i <= 10; i++) {
+    document.write(`<p>${j} x ${i} = ${j * i}</p>`);
+  }
+}
