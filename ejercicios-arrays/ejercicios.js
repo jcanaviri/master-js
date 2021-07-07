e80cc07189972b44d84338a0f8d78f11c5bfd8a0
jcanaviri <jcanaviri20@gmail.com>
2021-07-06 20:23:10 -0400

    Ordenando los elementos

--- ejercicios-arrays/ejercicios.js
+++ ejercicios-arrays/ejercicios.js
@@ -3,13 +3,21 @@
 // Pedir 6 numeros al usuario y agregarlos a un array
 let arr = new Array(6);
 
+const showArray = (heading, arr) => {
+  document.write(`<h3>${heading}</h3>`);
+  arr.forEach((num) => {
+    document.write(`<strong>${num}</strong></br>`);
+  });
+};
+
 for (let i = 0; i < arr.length; i++) {
   arr[i] = parseInt(prompt('Introduzca un numero: '));
 }
-// Mostramos el array por la consola
-console.log(arr);
+
 // Mostramos en el cuerpo de la página
-document.write('<h1>Elementos del array</h1>');
-arr.forEach((num, i) => {
-  document.write(`<strong>${num}</strong></br>`);
-});
+showArray('Elementos del array', arr);
+
+// Ordenamos el array
+arr.sort();
+console.log(arr);
+showArray('Ordenando los elementos', arr);
