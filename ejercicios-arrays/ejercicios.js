86f13fa11f7256437c66b34cb230b4d65c4c53b1
jcanaviri <jcanaviri20@gmail.com>
2021-07-06 20:15:54 -0400

    Mostramos el array en el DOM

--- ejercicios-arrays/ejercicios.js
+++ ejercicios-arrays/ejercicios.js
@@ -6,5 +6,10 @@ let arr = new Array(6);
 for (let i = 0; i < arr.length; i++) {
   arr[i] = parseInt(prompt('Introduzca un numero: '));
 }
-
+// Mostramos el array por la consola
 console.log(arr);
+// Mostramos en el cuerpo de la página
+document.write('<h1>Elementos del array</h1>');
+arr.forEach((num, i) => {
+  document.write(`<strong>${num}</strong></br>`);
+});
