84fb9f41488cc372ae865ad06ffdb9920db383ec
jcanaviri <jcanaviri20@gmail.com>
2021-07-07 13:13:16 -0400

    Evento load

--- eventos/main.js
+++ eventos/main.js
@@ -1,50 +1,52 @@
 'use strict';
 
-let btn = document.querySelector('#btn-color');
-let counter = 1;
+window.addEventListener('load', () => {
+  let btn = document.querySelector('#btn-color');
+  let counter = 1;
 
-btn.addEventListener('click', (event) => {
-  if (counter % 2 == 0) {
-    btn.style.background = 'red';
-  } else {
-    btn.style.background = 'green';
-  }
-  counter++;
-});
+  btn.addEventListener('click', (event) => {
+    if (counter % 2 == 0) {
+      btn.style.background = 'red';
+    } else {
+      btn.style.background = 'green';
+    }
+    counter++;
+  });
 
-let btnCounter = document.querySelector('#btn-counter');
-let counterNumber = 1;
+  let btnCounter = document.querySelector('#btn-counter');
+  let counterNumber = 1;
 
-btnCounter.style.borderRadius = '50%';
-btnCounter.style.padding = '30px';
+  btnCounter.style.borderRadius = '50%';
+  btnCounter.style.padding = '30px';
 
-btnCounter.addEventListener('click', (event) => {
-  btnCounter.textContent = counterNumber;
-  counterNumber++;
-});
+  btnCounter.addEventListener('click', (event) => {
+    btnCounter.textContent = counterNumber;
+    counterNumber++;
+  });
 
-// Focus
-let inputField = document.getElementById('field_name');
-inputField.addEventListener('focus', () => {
-  console.log('[focus] Esta en focus');
-});
+  // Focus
+  let inputField = document.getElementById('field_name');
+  inputField.addEventListener('focus', () => {
+    console.log('[focus] Esta en focus');
+  });
 
-// Blur
-inputField.addEventListener('blur', () => {
-  console.log('[blur] FUERA del input');
-});
+  // Blur
+  inputField.addEventListener('blur', () => {
+    console.log('[blur] FUERA del input');
+  });
 
-// Keydown
-inputField.addEventListener('keydown', (event) => {
-  console.log('pulsada tecla', String.fromCharCode(event.keyCode));
-});
+  // Keydown
+  inputField.addEventListener('keydown', (event) => {
+    console.log('pulsada tecla', String.fromCharCode(event.keyCode));
+  });
 
-// Keypress
-inputField.addEventListener('keypress', (event) => {
-  console.log('pulsando tecla', String.fromCharCode(event.keyCode));
-});
+  // Keypress
+  inputField.addEventListener('keypress', (event) => {
+    console.log('pulsando tecla', String.fromCharCode(event.keyCode));
+  });
 
-// Keyup
-inputField.addEventListener('keyup', (event) => {
-  console.log('levantaste el dedo', String.fromCharCode(event.keyCode));
+  // Keyup
+  inputField.addEventListener('keyup', (event) => {
+    console.log('levantaste el dedo', String.fromCharCode(event.keyCode));
+  });
 });
