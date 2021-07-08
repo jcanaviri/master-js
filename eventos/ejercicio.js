3589e3e0adb7b9bd09536a6437abdc6607843972
jcanaviri <jcanaviri20@gmail.com>
2021-07-07 21:35:45 -0400

    Ejercicio eventos

--- eventos/ejercicio.js
+++ eventos/ejercicio.js
@@ -1,5 +1,24 @@
 'use strict';
 
 window.addEventListener('load', () => {
-  
+  let form = document.querySelector('#form');
+  let dashedBox = document.querySelector('.dashed');
+
+  dashedBox.style.display = 'none';
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let name = document.querySelector('#name').value;
+    let surname = document.querySelector('#surname').value;
+    let age = document.querySelector('#age').value;
+    
+    let paragraph = document.createElement('p');
+    let userData = [name, surname, age];
+    userData.forEach(item => {
+      paragraph.innerHTML += `${item} <br>`;
+    })
+    dashedBox.appendChild(paragraph);
+    dashedBox.style.display = 'block';
+  });
 });
