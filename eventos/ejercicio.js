2007568434b38e88536b976813f71879cc019f6f
jcanaviri <jcanaviri20@gmail.com>
2021-07-07 21:44:12 -0400

    Simple validacion del formulario

--- eventos/ejercicio.js
+++ eventos/ejercicio.js
@@ -11,13 +11,25 @@ window.addEventListener('load', () => {
 
     let name = document.querySelector('#name').value;
     let surname = document.querySelector('#surname').value;
-    let age = document.querySelector('#age').value;
-    
+    let age = parseInt(document.querySelector('#age').value);
+
+    if (
+        name == null || 
+        surname == null || 
+        age == null ||
+        name.length == 0 || 
+        surname.length == 0 || 
+        age.length == 0 || isNaN(age)
+    ) {
+      alert('No llenaste bien los datos!');
+      return false;
+    }
+
     let paragraph = document.createElement('p');
     let userData = [name, surname, age];
-    userData.forEach(item => {
+    userData.forEach((item) => {
       paragraph.innerHTML += `${item} <br>`;
-    })
+    });
     dashedBox.appendChild(paragraph);
     dashedBox.style.display = 'block';
   });
