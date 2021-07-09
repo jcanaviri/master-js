86bd1765674ac31b78727fd22d5afac30e6a5748
jcanaviri <jcanaviri20@gmail.com>
2021-07-09 13:37:04 -0400

    Creating a resource

--- fetch/script.js
+++ fetch/script.js
@@ -26,3 +26,18 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
     ulPost.appendChild(p);
     loadingPost.style.display = 'none';
   });
+
+// Creating a resourse
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
