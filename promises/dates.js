9646aa5f07d54fd985603380752d2bcf0712807c
jcanaviri <jcanaviri20@gmail.com>
2021-07-10 12:08:51 -0400

    Using dates in JS

---
+++ promises/dates.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth();
+let day = now.getDate();
+let hour = now.getHours();
+
+console.log(now);
+console.log(year);
+console.log(month + 1);
+console.log(day);
+console.log(hour);
