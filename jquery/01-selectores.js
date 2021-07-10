155c8931325c429c7cdc4209bdb14afbb691448e
jcanaviri <jcanaviri20@gmail.com>
2021-07-10 14:40:30 -0400

    Jquery selectores de clase

--- jquery/01-selectores.js
+++ jquery/01-selectores.js
@@ -8,4 +8,13 @@ $(document).ready(() => {
   $('#green').css('background', 'green').css('color', '#fff');
 
   $('#blue').css('background', 'blue').css('color', '#fff');
+
+  // Selector de clase
+  let myClass = $('.my-class');
+  myClass.css('border', '5px dashed black');
+
+  $('.without-border').click(function() {
+    $(this).addClass('zebra');
+    console.log('CLICK');
+  });
 });
