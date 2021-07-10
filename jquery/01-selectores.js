e3a02066a2ff05d1289a9880a7ec0c7fa38564c1
jcanaviri <jcanaviri20@gmail.com>
2021-07-10 14:30:58 -0400

    Jquery selectores de ID

---
+++ jquery/01-selectores.js
@@ -0,0 +1,11 @@
+// Jquery o $ son lo mismo
+$(document).ready(() => {
+  console.log('JQuery works!');
+
+  // Selector de ID
+  $('#red').css('background', 'red').css('color', '#fff');
+
+  $('#green').css('background', 'green').css('color', '#fff');
+
+  $('#blue').css('background', 'blue').css('color', '#fff');
+});
