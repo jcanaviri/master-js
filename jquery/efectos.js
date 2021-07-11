393d452264f27fe7fec39ff71b361da8ab8f8836
jcanaviri <jcanaviri20@gmail.com>
2021-07-11 14:12:23 -0400

    Animaciones en Jquery

--- jquery/efectos.js
+++ jquery/efectos.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(document).ready(function () {
+  let box = $('#box');
   $('#show').hide();
 
   $('#show').click(function () {
@@ -18,5 +19,31 @@ $(document).ready(function () {
 
   $('#allInOne').click(function () {
     $('#box').slideToggle('fast');
+  });
+
+  $('#my-animation').click(function () {
+    box
+      .animate(
+        {
+          marginLeft: 500,
+          fontSize: '40px',
+          height: '110px',
+        },
+        'slow'
+      )
+      .animate(
+        {
+          borderRadius: '150px',
+          marginLeft: '0px',
+        },
+        'slow'
+      )
+      .animate(
+        {
+          borderRadius: '90px',
+          marginTop: '0px',
+        },
+        'slow'
+      );
   });
 });
