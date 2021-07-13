71f7f765972a0b48fb5afc355219e1f6a180d272
jcanaviri <jcanaviri20@gmail.com>
2021-07-13 14:38:39 -0400

    Iteracciones del usuario

--- jquery/interface.js
+++ jquery/interface.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
   console.log('Jquery works');
+  // Esto nos permite mover un elemento en la página
+  $('.elemento').draggable();
+
+  // Modificar el tamaño de los elementos
+  $('.elemento').resizable();
+
+  // $('.lista').selectable();
+  $('.lista').sortable({
+    update: function (event, ui) {
+      console.log('Has cambiando la lista');
+    },
+  });
+
+  // Drop
+  $('#elemento-movido').draggable();
+  $('#area').droppable({
+    drop: function (event) {
+      console.log('Has soltado algo dentro del area');
+    },
+  });
 });
