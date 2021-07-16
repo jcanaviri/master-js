72a3f37ef0186643f698e9ce4f9ed9b7f1a2c714
jcanaviri <jcanaviri20@gmail.com>
2021-07-15 22:42:42 -0400

    Creating a posts list

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,7 +1,33 @@
 $(document).ready(function () {
+  // Slider
   $('.bxslider').bxSlider({
     mode: 'fade',
     captions: true,
     slideWidth: 1200,
   });
+
+  // Posts
+  const posts = [
+    {
+      title: 'Prueba 1',
+      date: new Date(),
+      body: 'lorem ipsum dolor sit amet',
+    },
+    {
+      title: 'Prueba 2',
+      date: new Date(),
+      body: 'lorem ipsum dolor sit amet',
+    },
+    {
+      title: 'Prueba 3',
+      date: new Date(),
+      body: 'lorem ipsum dolor sit amet',
+    },
+    {
+      title: 'Prueba 4',
+      date: new Date(),
+      body: 'lorem ipsum dolor sit amet',
+    },
+  ];
+  console.log(posts);
 });
