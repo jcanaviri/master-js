3abd9faf1352ab3bdaedc14febf91ce0b244034a
jcanaviri <jcanaviri20@gmail.com>
2021-07-21 10:52:32 -0400

    Creando componentes con `ng generate component name`

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoGameComponent } from './video-game/video-game.component';
 import { ShoesComponent } from './shoes/shoes.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
-  declarations: [AppComponent, VideoGameComponent, ShoesComponent],
+  declarations: [AppComponent, VideoGameComponent, ShoesComponent, CoursesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
