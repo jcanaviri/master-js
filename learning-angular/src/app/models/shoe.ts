0a1958893d32edda82b36531a6d3a9b0531ecefa
jcanaviri <jcanaviri20@gmail.com>
2021-07-21 21:01:54 -0400

    Using a shoes model

---
+++ learning-angular/src/app/models/shoe.ts
@@ -0,0 +1,8 @@
+export class Shoe {
+  constructor(
+    public name: string,
+    public price: number,
+    public color: string,
+    public stock: boolean,
+  ) {}
+}
--- learning-angular/src/app/shoes/shoes.component.ts
+++ learning-angular/src/app/shoes/shoes.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Shoe } from '../models/shoe';
 
 @Component({
   selector: 'shoes',
   templateUrl: './shoes.component.html',
 })
-export class ShoesComponent {
+export class ShoesComponent implements OnInit {
   public title: string = 'Shoes Component';
+  public shoes: Array<Shoe> = [];
+
+  constructor() {
+    this.shoes = [
+      new Shoe('Nike Airmax', 700, 'Violeta', true),
+      new Shoe('Reebook Classic', 500, 'Blancas', true),
+      new Shoe('Nike Runner', 400, 'Negras', true),
+      new Shoe('Adidas Yezzy', 600, 'Purpura', false),
+    ];
+  }
+
+  ngOnInit() {
+    console.table(this.shoes);
+  }
 }
