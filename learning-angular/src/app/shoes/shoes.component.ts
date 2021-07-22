0983530b2585a6abf2fa4d5503d748379d0c4d7c
jcanaviri <jcanaviri20@gmail.com>
2021-07-21 22:42:46 -0400

    Angular directives

--- learning-angular/src/app/shoes/shoes.component.ts
+++ learning-angular/src/app/shoes/shoes.component.ts
@@ -4,21 +4,33 @@ import { Shoe } from '../models/shoe';
 @Component({
   selector: 'shoes',
   templateUrl: './shoes.component.html',
+  styleUrls: ['./shoes.component.css'],
 })
 export class ShoesComponent implements OnInit {
   public title: string = 'Shoes Component';
   public shoes: Array<Shoe> = [];
+  public names: string[] = [];
+  public color: string;
 
   constructor() {
+    this.color = 'blue';
     this.shoes = [
-      new Shoe('Nike Airmax', 700, 'Violeta', true),
-      new Shoe('Reebook Classic', 500, 'Blancas', true),
-      new Shoe('Nike Runner', 400, 'Negras', true),
-      new Shoe('Adidas Yezzy', 600, 'Purpura', false),
+      new Shoe('Nike Airmax', 400, 'Violeta', true),
+      new Shoe('Reebook Classic', 500, 'Blancas', false),
+      new Shoe('Nike Runner', 300, 'Negras', true),
+      new Shoe('Adidas Yezzy', 900, 'Purpura', true),
     ];
   }
 
   ngOnInit() {
-    console.table(this.shoes);
+    this.getNames();
+  }
+
+  getNames() {
+    this.shoes.forEach((shoe) => {
+      if (!this.names.includes(shoe.name)) {
+        this.names.push(shoe.name);
+      }
+    });
   }
 }
