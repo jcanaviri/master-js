c9cafaaa431a95becf0d6b283bc080981b0546fc
jcanaviri <jcanaviri20@gmail.com>
2021-07-25 18:36:40 -0400

    Conecting to the database

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,25 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect('mongodb://localhost:27017/portafolio', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Conected XD');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000...')
-})
+  console.log('Listening on port 3000...');
+});
