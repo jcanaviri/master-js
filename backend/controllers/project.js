b2a451663787604266ffea9ac10c72a658ee63d3
jcanaviri <jcanaviri20@gmail.com>
2021-07-25 22:10:04 -0400

    Add getProject and getProjects

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -8,11 +8,13 @@ const controller = {
       message: 'I am the home',
     });
   },
+
   test: function (req, res) {
     return res.send({
       message: 'I am the test',
     });
   },
+
   saveProject: function (req, res) {
     const newProject = new Project();
     let params = req.body;
@@ -42,7 +44,49 @@ const controller = {
 
       return res.status(200).send({ p });
     });
+  },
 
+  getProject: function (req, res) {
+    const projectId = req.params.id;
+
+    Project.findById(projectId, (err, p) => {
+      // Response with internal server error
+      // if some error occurred
+      if (err) {
+        return res.status(500).send({
+          message: 'Some error has occurred',
+        });
+      }
+
+      // If project not exists,
+      // we send not found status code
+      if (!p) {
+        return res.status(404).send({
+          message: 'Project has not been found',
+        });
+      }
+
+      return res.status(200).send({ p });
+    });
+  },
+
+  getProjects: function (req, res) {
+    Project.find({}).exec((err, projects) => {
+      if (err) {
+        return res.status(500).send({
+          message: 'Some error has occurred',
+        });
+      }
+
+      // If project not exists,
+      // we send not found status code
+      if (!projects)
+        return res.status(404).send({
+          message: 'Projects not found',
+        });
+
+      return res.status(200).send({ projects });
+    });
   },
 };
 
