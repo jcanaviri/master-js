6c2b44dc18ed5a489140e7ad3d43233502ad0ab5
jcanaviri <jcanaviri20@gmail.com>
2021-07-27 10:28:12 -0400

    Creating the routes of the proyect

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+import { routing, appRoutingProviders } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ErrorComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing,
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
