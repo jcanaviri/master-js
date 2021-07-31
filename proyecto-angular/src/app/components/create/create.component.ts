24562a15dfb3e63f41192b029103c0bd8f7abf76
jcanaviri <jcanaviri20@gmail.com>
2021-07-30 23:11:59 -0400

    Upload images in with Angular

--- proyecto-angular/src/app/components/create/create.component.ts
+++ proyecto-angular/src/app/components/create/create.component.ts
@@ -2,44 +2,63 @@ import { Component, OnInit } from '@angular/core';
 
 import { Project } from '../../models/project';
 import { ProjectService } from '../../services/project.service';
+import { UploadService } from '../../services/upload.service';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService],
 })
 export class CreateComponent implements OnInit {
-
   public title: string;
   public project: Project;
   public status: string;
-
+  public filesToUpload: Array<File> = new Array<File>();
 
   constructor(
     private _projectService: ProjectService,
+    private _uploadService: UploadService
   ) {
-    this.title = "Crear Projecto",
-    this.project = new Project('', '', '', '', 2021, '', '');
+    (this.title = 'Crear Projecto'),
+      (this.project = new Project('', '', '', '', 2021, '', ''));
     this.status = '';
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(form: any): void {
     this._projectService.saveProject(this.project).subscribe(
-      response => {
+      (response) => {
         if (response) {
-          this.status = 'success';
-          form.reset();
+          // Send the image
+          this._uploadService
+            .makeFileRequest(
+              `${environment.url}upload-img/${response.p._id}`,
+              [],
+              this.filesToUpload,
+              'image'
+            )
+            .then((result: any) => {
+              this.status = 'success';
+              console.log(result);
+              form.reset();
+            }).catch((error) => {
+              console.log(`SOME ERROR 👿: ${error}`)
+            });
         } else {
           this.status = 'failed';
         }
       },
-      error => {
+      (error) => {
         console.log(error);
       }
-    )
+    );
+  }
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 }
